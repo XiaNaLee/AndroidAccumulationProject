6b970da292498b786784fbbf4df734b36191c137
liyong <lee131483@gmail.com>
2015-07-09 19:05:21 +0800

    修改项目结构

--- app/src/com/accumulation/lee/androidaccumulationproject/activity/LoginActivity.java
+++ app/src/com/accumulation/lee/androidaccumulationproject/activity/LoginActivity.java
@@ -1,32 +1,15 @@
 package com.accumulation.lee.androidaccumulationproject.activity;
 
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.Animatable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.accumulation.lee.androidaccumulationproject.R;
-import com.accumulation.lee.utils.okhttp.OkHttpUtil;
-import com.accumulation.lee.utils.okhttp.OnDataReceviceListener;
 import com.facebook.cache.common.CacheKey;
 import com.facebook.common.internal.AndroidPredicates;
-import com.facebook.common.references.CloseableReference;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.BaseControllerListener;
-import com.facebook.drawee.controller.ControllerListener;
-import com.facebook.drawee.interfaces.DraweeController;
-import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory;
-import com.facebook.imagepipeline.common.ResizeOptions;
 import com.facebook.imagepipeline.core.ImagePipelineFactory;
-import com.facebook.imagepipeline.request.BasePostprocessor;
-import com.facebook.imagepipeline.request.ImageRequest;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
-import com.facebook.imagepipeline.request.Postprocessor;
 
 import in.srain.cube.views.ptr.PtrClassicFrameLayout;
 import in.srain.cube.views.ptr.PtrFrameLayout;
@@ -35,9 +18,7 @@ import in.srain.cube.views.ptr.PtrHandler;
 
 public class LoginActivity extends BaseActivity {
     private PtrClassicFrameLayout ptrFrameLayout;
-    private SimpleDraweeView simpleDraweeView;
-    private String testUrl = "http://img4.imgtn.bdimg.com/it/u=4274835636,2877046532&fm=21&gp=0.jpg";
-    private TextView tv;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,43 +38,9 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void findViews() {
         ptrFrameLayout = getView(R.id.store_house_ptr_frame);
-        simpleDraweeView = getView(R.id.my_image_view);
-        tv = getView(R.id.tv);
+        webView=getView(R.id.web_view);
     }
 
-    private String url = "http://www.joes-hardware.com/tools.html";
-    private boolean isStartRQ = true;
-
-    public void click(View view) {
-        if (isStartRQ) {
-            OkHttpUtil.httpGet(url, new OnDataReceviceListener() {
-                @Override
-                public void onSuccess(String url, final String jsonResponse) {
-                    simpleDraweeView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(LoginActivity.this, "" + jsonResponse, Toast.LENGTH_SHORT).show();
-                            tv.setText(jsonResponse);
-                        }
-                    });
-                }
-
-                @Override
-                public void onFailed(String url) {
-                    simpleDraweeView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(LoginActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            });
-            isStartRQ = false;
-        } else {
-            isStartRQ = true;
-            OkHttpUtil.cancelRequest(url);
-        }
-    }
 
     @Override
     protected void initDataBeforeSetViews() {
@@ -103,11 +50,16 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void setViews() {
-        //设置宽高比
-        simpleDraweeView.setAspectRatio(1.33f);
-        Uri uri = Uri.parse(testUrl);
-        // simpleDraweeView.setImageURI(uri);
-        setImageDraweeController(uri);
+        webView.loadUrl("https://www.baidu.com");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
         ptrFrameLayout.setPtrHandler(new PtrHandler() {
             @Override
             public void onRefreshBegin(PtrFrameLayout frame) {
@@ -121,17 +73,24 @@ public class LoginActivity extends BaseActivity {
 
             @Override
             public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
-                return true;
+                if(webView.getScrollY()==0){
+                    return true;
+                }else{
+                    return false;
+                }
             }
         });
-        // the following are default settings
+        //阻尼系数
         ptrFrameLayout.setResistance(1.7f);
+        //触发刷新时移动的位置比例.默认，1.2f，移动达到头部高度1.2倍时可触发刷新操作
         ptrFrameLayout.setRatioOfHeaderHeightToRefresh(1.2f);
+        //回弹延迟.默认 200ms，回弹到刷新高度所用时间
         ptrFrameLayout.setDurationToClose(200);
+        //头部回弹时间
         ptrFrameLayout.setDurationToCloseHeader(1000);
-        // default is false
+        //下拉刷新 / 释放刷新
         ptrFrameLayout.setPullToRefresh(false);
-        // default is true
+        //刷新是保持头部
         ptrFrameLayout.setKeepHeaderWhenRefresh(true);
         ptrFrameLayout.setEnabledNextPtrAtOnce(true);
 
@@ -146,89 +105,6 @@ public class LoginActivity extends BaseActivity {
 
     }
 
-    private void setImageDraweeController(Uri uri) {
-        //修改图片
-        Postprocessor myPostprocessor = new BasePostprocessor() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-
-            @Override
-            public void process(Bitmap bitmap) {
-                super.process(bitmap);
-                for (int x = 0; x < bitmap.getWidth(); x += 2) {
-                    for (int y = 0; y < bitmap.getHeight(); y += 2) {
-                        bitmap.setPixel(x, y, Color.RED);
-                    }
-                }
-            }
-
-            @Override
-            public void process(Bitmap destBitmap, Bitmap sourceBitmap) {
-                super.process(destBitmap, sourceBitmap);
-            }
-
-            @Override
-            public CloseableReference<Bitmap> process(Bitmap sourceBitmap, PlatformBitmapFactory bitmapFactory) {
-                return super.process(sourceBitmap, bitmapFactory);
-            }
-        };
-
-        ImageRequest request = ImageRequestBuilder
-                .newBuilderWithSource(uri)
-                .setAutoRotateEnabled(true)
-                .setLowestPermittedRequestLevel(ImageRequest.RequestLevel.FULL_FETCH)//允许设置一个最低请求级别
-                .setResizeOptions(new ResizeOptions(50, 50))
-                .setPostprocessor(myPostprocessor)
-                .setProgressiveRenderingEnabled(true)
-                .build();
-
-        //监听下载事件,对所有的图片加载，onFinalImageSet 或者 onFailure 都会被触发。前者在成功时，后者在失败时。
-        ControllerListener listener = new BaseControllerListener() {
-            @Override
-            public void onFailure(String id, Throwable throwable) {
-                super.onFailure(id, throwable);
-            }
-
-            @Override
-            public void onFinalImageSet(String id, Object imageInfo, Animatable animatable) {
-                super.onFinalImageSet(id, imageInfo, animatable);
-            }
-
-            @Override
-            public void onIntermediateImageFailed(String id, Throwable throwable) {
-                super.onIntermediateImageFailed(id, throwable);
-            }
-
-            @Override
-            public void onIntermediateImageSet(String id, Object imageInfo) {
-                super.onIntermediateImageSet(id, imageInfo);
-            }
-
-            @Override
-            public void onRelease(String id) {
-                super.onRelease(id);
-            }
-
-            @Override
-            public void onSubmit(String id, Object callerContext) {
-                super.onSubmit(id, callerContext);
-            }
-        };
-
-        DraweeController controller = Fresco.newDraweeControllerBuilder()
-                .setImageRequest(request)
-                .setAutoPlayAnimations(true)
-                        // .setUri(Uri.parse(url))
-                .setTapToRetryEnabled(true)
-                .setOldController(simpleDraweeView.getController())//在指定一个新的controller的时候，使用setOldController，这可节省不必要的内存分配。
-                .setControllerListener(listener)
-                .build();
-
-        simpleDraweeView.setController(controller);
-
-    }
 
     @Override
     public void onBackPressed() {
--- app/src/com/accumulation/lee/androidaccumulationproject/activity/MainActivity.java
+++ app/src/com/accumulation/lee/androidaccumulationproject/activity/MainActivity.java
@@ -1,11 +1,27 @@
 package com.accumulation.lee.androidaccumulationproject.activity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.accumulation.lee.androidaccumulationproject.R;
+import com.facebook.rebound.BaseSpringSystem;
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringSystem;
+import com.facebook.rebound.SpringUtil;
 
 /**
  * Created by liyong on 15/5/11.
  */
 public class MainActivity extends BaseActivity {
+    private ImageView imageView;
+    private final BaseSpringSystem mSpringSystem = SpringSystem.create();
+    private final ExampleSpringListener mSpringListener = new ExampleSpringListener();
+    private Spring mScaleSpring;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,22 +34,81 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected int getContentViewId() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @Override
     protected void findViews() {
-
+        imageView = getView(R.id.iv_click);
     }
 
     @Override
     protected void initDataBeforeSetViews() {
-
+        // Create the animation spring.
+        mScaleSpring = mSpringSystem.createSpring();
     }
 
 
     @Override
     protected void setViews() {
+        // Add an OnTouchListener to the root view.
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                long time = 0;
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // When pressed start solving the spring to 1.
+                        mScaleSpring.setEndValue(1);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        imageView.performClick();
+                        mScaleSpring.setEndValue(0);
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        // When released start solving the spring to 0.
+                        mScaleSpring.setEndValue(0);
+                        break;
 
+                }
+                return true;
+            }
+        });
+     imageView.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Toast.makeText(MainActivity.this,"click",Toast.LENGTH_SHORT).show();
+         }
+     });
+
+    }
+
+    private class ExampleSpringListener extends SimpleSpringListener {
+        @Override
+        public void onSpringUpdate(Spring spring) {
+            // On each update of the spring value, we adjust the scale of the image view to match the
+            // springs new value. We use the SpringUtil linear interpolation function mapValueFromRangeToRange
+            // to translate the spring's 0 to 1 scale to a 100% to 50% scale range and apply that to the View
+            // with setScaleX/Y. Note that rendering is an implementation detail of the application and not
+            // Rebound itself. If you need Gingerbread compatibility consider using NineOldAndroids to update
+            // your view properties in a backwards compatible manner.
+            float mappedValue = (float) SpringUtil.mapValueFromRangeToRange(spring.getCurrentValue(), 0, 1, 1, 0.5);
+            imageView.setScaleX(mappedValue);
+            imageView.setScaleY(mappedValue);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Add a listener to the spring when the Activity resumes.
+        mScaleSpring.addListener(mSpringListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Remove the listener to the spring when the Activity pauses.
+        mScaleSpring.removeListener(mSpringListener);
     }
 }
